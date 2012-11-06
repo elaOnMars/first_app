1856a2add135d9c66dd715df528e3dda5d0575d9
ElaOnMars <hallo@datentupel.de>
2012-11-06 21:09:39 +0100

    Finish package before uploading to git ... because online is also an commit with No. 1 - haha...

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106191154) do
+ActiveRecord::Schema.define(:version => 20121106191739) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "location"
   end
 
 end
