11eaac2143bbfa96e8dd033422d3e349cef1ad33
ElaOnMars <hallo@datentupel.de>
2012-11-09 10:42:23 +0100

    Users may create multiple microposts. Association between microposts and users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,12 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+<<<<<<< HEAD
   
-  # A micropost belongs to a user
+  # Micropost belongs to a user
   belongs_to :user
 
   # Enforcing the length of the micropost at most 140 characters
   validates :content, :length => { :maximum => 140}  
+=======
+>>>>>>> parent of a0d0f92... Enforcing the length of the micropost at most 30 characters
 end
