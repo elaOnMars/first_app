17cf1157e973d2b1b4eb49b1aca334c140b8781c
ElaOnMars <hallo@datentupel.de>
2012-11-09 11:01:16 +0100

    Finish first app - demo

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,12 +1,9 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
-<<<<<<< HEAD
   
   # Micropost belongs to a user
   belongs_to :user
 
   # Enforcing the length of the micropost at most 140 characters
-  validates :content, :length => { :maximum => 140}  
-=======
->>>>>>> parent of a0d0f92... Enforcing the length of the micropost at most 30 characters
+  validates :content, :length => { :maximum => 140}
 end
