a0d0f92b2a02f169f9c25caf6dd4f8baeb78181b
ElaOnMars <hallo@datentupel.de>
2012-11-09 10:29:16 +0100

    Enforcing the length of the micropost at most 30 characters

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  # Enforcing the length of the micropost at most 30 characters
+  validates :content, :length => { :maximum => 30}
 end
